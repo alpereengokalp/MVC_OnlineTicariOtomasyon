cb8842c4b16b19a24745e7696cf03b568099500d
Yusuf Alperen GÖKALP <alperengokalpp@gmail.com>
2024-06-05 00:18:20 +0300

    Cari kısmı tamamlandı

---
+++ Controllers/CariController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcOnlineTicariOtomasyon.Models.Siniflar;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class CariController : Controller
+    {
+        // GET: Cari
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var musteriler = c.Carilers.Where(x => x.Durum == true).ToList();
+            return View(musteriler);
+        }
+
+        [HttpGet]
+        public ActionResult MusteriEkle()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult MusteriEkle(Cariler m)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("MusteriGetir");
+            }
+            m.Durum = true;
+            c.Carilers.Add(m);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult MusteriSil(int id) 
+        {
+            var musteriSil = c.Carilers.Find(id);
+            musteriSil.Durum = false;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult MusteriGetir(int id)
+        {
+            var musteriGetir = c.Carilers.Find(id);
+            return View("MusteriGetir",musteriGetir);
+                   //MusteriGetir view adı, değişken
+        }
+
+        public ActionResult MusteriGuncelle(Cariler p)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("MusteriGetir");
+            }
+            var musteri = c.Carilers.Find(p);
+            musteri.CariAdi = p.CariAdi;
+            musteri.CariSoyadi = p.CariSoyadi;
+            musteri.CariSehir = p.CariSehir;
+            musteri.CariMail = p.CariMail;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult MusteriSatis(int id)
+        {
+            var musteriSatis = c.SatisHarekets.Where(x=>x.CariId==id).ToList();
+            var mSatis = c.Carilers.Where(x => x.CariID == id).Select(y => y.CariAdi + " " + y.CariSoyadi).FirstOrDefault();
+            ViewBag.musteriView = mSatis;
+            return View(musteriSatis);
+        }
+    }
+}
--- Models/Siniflar/Cariler.cs
+++ Models/Siniflar/Cariler.cs
@@ -13,22 +13,23 @@ namespace MvcOnlineTicariOtomasyon.Models.Siniflar
         public int CariID { get; set; }
 
         [Column(TypeName = "Varchar")]
-        [StringLength(30)]
+        [StringLength(30,ErrorMessage ="En fazla 30 karakter yazabilirsiniz")]
         public string CariAdi { get; set; }
 
         [Column(TypeName = "Varchar")]
-        [StringLength(30)]
+        [StringLength(30, ErrorMessage = "En fazla 30 karakter yazabilirsiniz")]
+        [Required(ErrorMessage ="Bu alanı boş geçemezsiniz")]
         public string CariSoyadi { get; set; }
 
         [Column(TypeName = "Varchar")]
-        [StringLength(13)]
+        [StringLength(13, ErrorMessage = "En fazla 13 karakter yazabilirsiniz")]
         public string CariSehir { get; set; }
 
         [Column(TypeName = "Varchar")]
-        [StringLength(50)]
+        [StringLength(50, ErrorMessage = "En fazla 50 karakter yazabilirsiniz")]
         public string CariMail { get; set; }
 
-
+        public bool Durum {  get; set; }
         /* İlişkilerin Kurulması */
         public ICollection<SatisHareket> SatisHarekets { get; set; } //Bir carinin birden fazla ürün alabilir.
     }
